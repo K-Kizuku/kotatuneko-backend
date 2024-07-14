720e0b9f808fdbdd915475750db2baf7d6028496
K-Kizuku <kotani.kizuku@gmail.com>
2024-07-14 21:38:01 +0900

    feat: 🍻 init WebSocket

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,44 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer conn.Close()
+
+	for {
+		messageType, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		if err := conn.WriteMessage(messageType, message); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
 func main() {
-	mux := http.NewServeMux()
-	// hello worldを返すエンドポイントの作成
-	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World!")
-	})
+	r := http.NewServeMux()
+	r.HandleFunc("/echo", echo)
 
-	// サーバーの起動
-	http.ListenAndServe(":8080", nil)
-
+	log.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		log.Fatal(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/K-Kizuku/kotatuneko-backend
 
 go 1.23rc1
+
+require (
+	github.com/gorilla/websocket v1.5.3
+	google.golang.org/protobuf v1.34.2
+)
+
+require github.com/K-Kizuku/kotatuneko-protobuf v0.1.3 // indirect
